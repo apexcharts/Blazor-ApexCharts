e95d2441a0b4cf4daffdeb3de521c9bf62c6e675
Joakim Dangården <Joakim.Dangarden@swegon.com>
2020-05-24 23:31:00 +0200

    some work on Gagues

---
+++ src/Blazor-ApexCharts/ApexGauge.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexCharts
+{
+    public partial class ApexGauge : IDisposable
+    {
+        [Parameter] public string Title { get; set; }
+        [Parameter] public decimal Precentage { get; set; }
+        [Parameter] public string Label { get; set; }
+        [Parameter] public ApexChartOptions<GaugeValue> Options { get; set; } = new ApexChartOptions<GaugeValue>();
+
+        private ApexChartOptions<GaugeValue> options;
+        private ApexChart<GaugeValue> chart;
+
+        protected override void OnInitialized()
+        {
+            options = Options;
+        }
+
+      
+
+        private List<GaugeValue> GetItems()
+        {
+            return new List<GaugeValue> { new GaugeValue { Label = Label, Precentage = Precentage } };
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
