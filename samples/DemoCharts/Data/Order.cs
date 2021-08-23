0a7672f86174c2fbdc59daf5cc290f173be05817
Joakim Dangården <Joakim.Dangarden@swegon.com>
2021-08-23 22:37:14 +0200

    Update to .net 5

---
+++ samples/DemoCharts/Data/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DemoCharts
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; } = Guid.NewGuid();
+        public string CustomerName { get; set; }
+        public string Country { get; set; }
+        public DateTimeOffset OrderDate { get; set; }
+        public OrderType OrderType { get; set; }
+        public decimal GrossValue { get; set; }
+        public decimal NetValue { get =>  GrossValue * (1 - (DiscountPrecentage / 100)) ; }
+        public decimal DiscountPrecentage { get; set; }
+    }
+
+    public enum OrderType
+    {
+        Web, Contract, Mail, Phone
+    }
+}
