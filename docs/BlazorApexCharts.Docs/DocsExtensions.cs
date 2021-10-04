d932e6621e61ee6fa4d60c1cf321df9ad2831155
Joakim Dangården <Joakim.Dangarden@swegon.com>
2021-10-04 21:15:54 +0200

    First stab at new docs

---
+++ docs/BlazorApexCharts.Docs/DocsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TabBlazor;
+
+namespace BlazorApexCharts.Docs
+{
+    public static class DocsExtensions
+    {
+        public static IServiceCollection AddDocs(this IServiceCollection services)
+        {
+            return services
+               .AddTabler();
+             }
+    }
+}
