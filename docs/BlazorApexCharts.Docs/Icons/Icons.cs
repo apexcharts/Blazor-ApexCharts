002ce5c6a0f7a04b256786f4762406c0a1f8bf98
Joakim Dangården <Joakim.Dangarden@swegon.com>
2021-10-07 21:52:01 +0200

    More docs

--- docs/BlazorApexCharts.Docs/Icons/Icons.cs
+++ docs/BlazorApexCharts.Docs/Icons/Icons.cs
@@ -12,6 +12,8 @@ namespace BlazorApexCharts.Docs
         public static string Chart_donut { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M10 3.2a9 9 0 1 0 10.8 10.8a1 1 0 0 0 -1 -1h-3.8a4.1 4.1 0 1 1 -5 -5v-4a0.9 .9 0 0 0 -1 -.8' /><path d='M15 3.5a9 9 0 0 1 5.5 5.5h-4.5a9 9 0 0 0 -1 -1v-4.5' />"; }
         public static string Chart_line { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><line x1='4' y1='19' x2='20' y2='19' /><polyline points='4 15 8 9 12 11 16 6 20 10' />"; }
         public static string Chart_infographic { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><circle cx='7' cy='7' r='4' /><path d='M7 3v4h4' /><line x1='9' y1='17' x2='9' y2='21' /><line x1='17' y1='14' x2='17' y2='21' /><line x1='13' y1='13' x2='13' y2='21' /><line x1='21' y1='12' x2='21' y2='21' />"; }
+        public static string Brand_github { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5' />"; }
+        public static string Chart_bar { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><rect x='3' y='12' width='6' height='8' rx='1' /><rect x='9' y='8' width='6' height='12' rx='1' /><rect x='15' y='4' width='6' height='16' rx='1' /><line x1='4' y1='20' x2='18' y2='20' />"; }
 
     }
 }
