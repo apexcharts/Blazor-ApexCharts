cbfdebe0eefb8857a0367d2f3613a813f19d9bda
Joakim Dangården <Joakim.Dangarden@swegon.com>
2021-10-07 22:19:24 +0200

    Addad docs wasm project

---
+++ docs/BlazorApexCharts.Docs.Wasm/Program.cs
@@ -0,0 +1,27 @@
+using BlazorApexCharts.Docs.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApexCharts.Docs.Wasm
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddDocs();
+            builder.Services.AddScoped<ICodeSnippetService, FakeSnippetService>();
+            await builder.Build().RunAsync();
+        }
+    }
+}
