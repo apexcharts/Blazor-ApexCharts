1fbe8d0a9fa0f193f34a0bbffd5de7d4392958eb
Joakim Dangården <Joakim.Dangarden@swegon.com>
2023-02-05 13:14:12 +0100

    Added XAxisLabelClick

---
+++ src/Blazor-ApexCharts/Series/XAxisLabelClicked.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ApexCharts
+{
+    public class XAxisLabelClicked<TItem> where TItem : class
+    {
+        public int LabelIndex { get; set; }
+        public string Caption { get; set; }
+        public List<SeriesDataPoint<TItem>> SeriesPoints { get; set; }
+
+    }
+
+    public class SeriesDataPoint<TItem> where TItem : class
+    {
+        public Series<TItem> Series { get; set; }
+        public IDataPoint<TItem> DataPoint { get; set; }
+  
+    }
+
+}
