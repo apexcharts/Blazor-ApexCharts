4bb15d20546a38aea96bed2780dd79b1e1e075d7
Joakim Dangården <Joakim.Dangarden@swegon.com>
2024-04-06 10:12:22 +0200

    Added the option to pass in javascript path

---
+++ src/Blazor-ApexCharts/Models/BlazorOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexCharts
+{
+    /// <summary>
+    /// Options for the blazor component. 
+    /// </summary>
+    public class ApexChartsBlazorOptions
+    {
+        /// <summary>
+        /// Alternative File path to the Blazor javascript file, blazor-apexcharts.js
+        /// This must be the full path including filename and the file apexcharts.esm.js must be in the same folder
+        /// /// </summary>
+        public string JavascriptPath { get; set; }
+    }
+}
