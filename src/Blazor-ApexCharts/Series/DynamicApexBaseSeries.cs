8b4ad62afa6588d8bbd35584ff63ee5240fd593d
jbaumflek <jbaumflek@hotmail.com>
2024-08-27 10:00:45 -0400

    Code-only series creation/manipulation
    While v2 introduced dynamic series of a sort, I have the need to add series completely from the back end.  The only difference with the Dynamic* series classes is that they don't rely on parameterized settings.

---
+++ src/Blazor-ApexCharts/Series/DynamicApexBaseSeries.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApexCharts
+{
+    /// <summary>
+    /// Base class to create a data series for a chart
+    /// </summary>
+    /// <typeparam name="TItem">The data type to be used in the chart to create data points.</typeparam>
+    public abstract class DynamicApexBaseSeries<TItem> : ComponentBase where TItem : class
+    {
+        /// <inheritdoc cref="IApexSeries{TItem}.Chart"/>
+        public ApexChart<TItem> Chart { get; set; }
+
+        /// <inheritdoc cref="IApexSeries{TItem}.Name"/>
+        public string Name { get; set; }
+
+        /// <inheritdoc cref="IApexSeries{TItem}.XValue"/>
+        public Func<TItem, object> XValue { get; set; }
+
+        /// <inheritdoc cref="IApexSeries{TItem}.ShowDataLabels"/>
+        public bool ShowDataLabels { get; set; }
+
+        /// <inheritdoc cref="IApexSeries{TItem}.Items"/>
+        public IEnumerable<TItem> Items { get; set; }
+
+        /// <inheritdoc cref="IApexSeries{TItem}.Stroke"/>
+        public SeriesStroke Stroke { get; set; }
+
+        /// <inheritdoc cref="IApexSeries{TItem}.Color"/>
+        public string Color { get; set; }
+
+        /// <summary>
+        /// A function to provide a custom color for data points in the series
+        /// </summary>
+        /// <remarks>
+        /// Links:
+        ///
+        /// <see href="https://apexcharts.com/docs/options/colors">JavaScript Reference</see>
+        /// </remarks>
+        public Func<TItem, string> PointColor { get; set; }
+		
+        /// <inheritdoc cref="IApexSeries{TItem}.Group"/>
+		public string Group { get; set; }
+
+        /// <inheritdoc cref="IApexSeries{TItem}.Toggle"/>
+        public async Task Toggle()
+        {
+            await Chart?.ToggleSeriesAsync(Name);
+        }
+
+        /// <inheritdoc cref="IApexSeries{TItem}.Show"/>
+        public async Task Show()
+        {
+            await Chart?.ShowSeriesAsync(Name);
+        }
+
+        /// <inheritdoc cref="IApexSeries{TItem}.Hide"/>
+        public async Task Hide()
+        {
+            await Chart?.HideSeriesAsync(Name);
+        }
+
+        /// <summary>
+        /// Executes the <see cref="PointColor"/> function on the provided data point and returns the color value
+        /// </summary>
+        /// <param name="item">The data point to return the color for</param>
+        public string GetPointColor(TItem item)
+        {
+            if (PointColor == null || item == null) { return null; }
+            return PointColor.Invoke(item);
+        }
+
+        /// <summary>
+        /// Executes the <see cref="PointColor"/> function on the provided data points and returns the color value
+        /// </summary>
+        /// <param name="items">The data points to return the color for</param>
+        /// <remarks>
+        /// Only returns the color for the first item in the collection
+        /// </remarks>
+        public string GetPointColor(IEnumerable<TItem> items)
+        {
+            if (PointColor == null || items == null || !items.Any()) { return null; }
+            return PointColor.Invoke(items.First());
+        }
+
+        internal List<T> UpdateDataPoints<T>(IEnumerable<T> items, Action<T> updateMethod)
+        {
+            var data = items.ToList();
+
+            if (updateMethod == null)
+            {
+                return data;
+            }
+
+            foreach (T item in data)
+            {
+                updateMethod(item);
+            }
+
+            return data;
+        }
+    }
+}
---
+++ src/Blazor-ApexCharts/Series/DynamicApexPointSeries.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApexCharts
+{
+    /// <summary>
+    /// Component to create various data series types in Blazor
+    /// </summary>
+    /// <typeparam name="TItem">The data type to be used in the chart to create data points.</typeparam>
+    /// <remarks>
+    /// Links:
+    /// 
+    /// <see href="https://apexcharts.com/docs/chart-types">JavaScript Documentation</see>
+    /// </remarks>
+    public class DynamicApexPointSeries<TItem> : DynamicApexBaseSeries<TItem>, IApexSeries<TItem> where TItem : class
+    {
+        /// <summary>
+        /// Expression to get the Y-value for each X-Value. Use when a single Y-value is available.
+        /// </summary>
+        public Func<TItem, decimal?> YValue { get; set; }
+
+        /// <summary>
+        /// Expression to group Y-values for each X-Value. Use when a multiple Y-values are available.
+        /// </summary>
+        /// <remarks>
+        /// Will be ignored if <see cref="YValue"/> is set
+        /// </remarks>
+        public Func<IEnumerable<TItem>, decimal?> YAggregate { get; set; }
+
+        /// <summary>
+        /// Expression to determine the ordering of X-Values in the series
+        /// </summary>
+        public Func<DataPoint<TItem>, object> OrderBy { get; set; }
+
+        /// <summary>
+        /// Expression to determine the inverse ordering of X-Values in the series
+        /// </summary>
+        public Func<DataPoint<TItem>, object> OrderByDescending { get; set; }
+
+        /// <summary>
+        /// Determines the type of data series to draw on the chart
+        /// </summary>
+        public SeriesType SeriesType { get; set; }
+
+        /// <summary>
+        /// Function to conditionally modify individual data points in the series
+        /// </summary>
+        public Action<DataPoint<TItem>> DataPointMutator { get; set; }
+
+        /// <inheritdoc/>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Chart.AddSeries(this);
+        }
+
+        /// <inheritdoc/>
+        public ChartType GetChartType()
+        {
+            switch (SeriesType)
+            {
+                case SeriesType.Area:
+                    return ChartType.Area;
+                case SeriesType.Bar:
+                    return ChartType.Bar;
+                case SeriesType.Donut:
+                    return ChartType.Donut;
+                case SeriesType.Heatmap:
+                    return ChartType.Heatmap;
+                case SeriesType.Line:
+                    return ChartType.Line;
+                case SeriesType.Pie:
+                    return ChartType.Pie;
+                case SeriesType.PolarArea:
+                    return ChartType.PolarArea;
+                case SeriesType.Radar:
+                    return ChartType.Radar;
+                case SeriesType.RadialBar:
+                    return ChartType.RadialBar;
+                case SeriesType.Scatter:
+                    return ChartType.Scatter;
+                case SeriesType.Treemap:
+                    return ChartType.Treemap;
+                case SeriesType.RangeArea:
+                    return ChartType.RangeArea;
+                default:
+                    throw new SystemException($"SeriesType {SeriesType} can not be converted to ChartType");
+            }
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<IDataPoint<TItem>> GenerateDataPoints(IEnumerable<TItem> items)
+        {
+            if (items == null)
+            {
+                return Enumerable.Empty<IDataPoint<TItem>>();
+            }
+
+            IEnumerable<DataPoint<TItem>> data;
+
+            if (YValue != null)
+            {
+                data = items.Select(e => new DataPoint<TItem>
+                {
+                    X = XValue.Invoke(e),
+                    Y = YValue.Invoke(e),
+                    Items = new List<TItem> { e },
+                    FillColor = GetPointColor(e)
+                });
+
+            }
+            else if (YAggregate != null)
+            {
+                data = items.GroupBy(XValue)
+               .Select(d => new DataPoint<TItem>
+               {
+                   X = d.Key,
+                   Y = YAggregate.Invoke(d),
+                   Items = d.ToList(),
+                   FillColor = GetPointColor(d)
+               });
+            }
+            else
+            {
+                return new List<IDataPoint<TItem>>();
+            }
+
+            data = GroupData(data.ToList());
+
+            if (OrderBy != null)
+            {
+                data = data.OrderBy(OrderBy);
+            }
+            else if (OrderByDescending != null)
+            {
+                data = data.OrderByDescending(OrderByDescending);
+            }
+
+            return UpdateDataPoints(data, DataPointMutator);
+        }
+
+        private List<DataPoint<TItem>> GroupData(List<DataPoint<TItem>> dataPoints)
+        {
+            if (Chart.GroupPoints == null || !dataPoints.Any())
+            {
+                return dataPoints;
+            }
+
+            if (Chart.XAxisType != XAxisType.Category)
+            {
+                throw new Exception("If chart GroupValues is specified then XAxisType must be set to Category.");
+            }
+
+            if (Chart.GroupPoints.PercentageThreshold == null && Chart.GroupPoints.MaxCount == null)
+            {
+                return dataPoints;
+            }
+
+            var newData = new List<DataPoint<TItem>>();
+            decimal? thresholdValue = null;
+            var maxCount = Chart.GroupPoints.MaxCount;
+            int currentCount = 0;
+
+            var groupedPoint = new DataPoint<TItem>
+            {
+                GroupedPoints = new List<DataPoint<TItem>>()
+            };
+
+            if (Chart.GroupPoints.PercentageThreshold != null)
+            {
+                thresholdValue = (decimal)(dataPoints.Sum(e => e.Y) * ((decimal)Chart.GroupPoints.PercentageThreshold / 100));
+            }
+
+            foreach (var dataPoint in dataPoints.OrderByDescending(e => e.Y))
+            {
+                if (ShouldGroup(maxCount, currentCount, dataPoint.Y, thresholdValue))
+                {
+                    groupedPoint.GroupedPoints.Add(dataPoint);
+                }
+                else
+                {
+                    newData.Add(dataPoint);
+                    currentCount++;
+                }
+            }
+
+            if (groupedPoint.GroupedPoints.Any())
+            {
+                groupedPoint.X = Chart.GroupPoints.Name;
+                groupedPoint.Y = groupedPoint.GroupedPoints.Sum(e => e.Y);
+                newData.Add(groupedPoint);
+            }
+
+            return newData;
+        }
+
+        private bool ShouldGroup(int? maxCount, int currentCount, decimal? currentValue, decimal? thresholdValue)
+        {
+            if (maxCount == null && thresholdValue == null)
+            {
+                return false;
+            }
+
+            if (maxCount != null && currentCount >= maxCount)
+            {
+                return true;
+            }
+
+            if (thresholdValue != null && currentValue < thresholdValue)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            Chart.RemoveSeries(this);
+        }
+    }
+}
