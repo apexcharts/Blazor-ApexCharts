972097c732b72e96d6093fd4aa2331bfaf5c1b55
Joakim Dangården <Joakim.Dangarden@swegon.com>
2024-10-19 14:41:20 +0200

    #522

--- src/Blazor-ApexCharts/wwwroot/js/blazor-apexcharts.js
+++ src/Blazor-ApexCharts/wwwroot/js/blazor-apexcharts.js
@@ -1,4 +1,4 @@
-﻿import ApexCharts from './apexcharts.esm.js?ver=3.54.0'
+﻿import ApexCharts from './apexcharts.esm.js?ver=3.54.1'
 
 // export function for Blazor to point to the window.blazor_apexchart. To be compatible with the most JS Interop calls the window will be return.
 export function get_apexcharts() {
