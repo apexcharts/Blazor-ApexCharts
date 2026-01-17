b0755e996469f8b2134691d07544963157ded739
Joakim Dangården <Joakim.Dangarden@swegon.com>
2026-01-17 12:42:15 +0100

    Fixed icons in docs

--- docs/BlazorApexCharts.Docs/Icons/Icons.cs
+++ docs/BlazorApexCharts.Docs/Icons/Icons.cs
@@ -5,6 +5,8 @@ namespace BlazorApexCharts.Docs
     public static class Icons
     {
         public static IIconType Home { get => new TablerIcon(@"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><polyline points='5 12 3 12 12 3 21 12 19 12' /><path d='M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7' /><path d='M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6' />"); }
+        public static IIconType Chart_treemap => new TablerIcon(@"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z' /><path d='M12 4v16' /><path d='M4 15h8' /><path d='M12 12h8' /><path d='M16 12v8' /><path d='M16 16h4' />");
+
         public static IIconType Chart_donut { get => new TablerIcon(@"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M10 3.2a9 9 0 1 0 10.8 10.8a1 1 0 0 0 -1 -1h-3.8a4.1 4.1 0 1 1 -5 -5v-4a0.9 .9 0 0 0 -1 -.8' /><path d='M15 3.5a9 9 0 0 1 5.5 5.5h-4.5a9 9 0 0 0 -1 -1v-4.5' />"); }
         public static IIconType Chart_line { get => new TablerIcon(@"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><line x1='4' y1='19' x2='20' y2='19' /><polyline points='4 15 8 9 12 11 16 6 20 10' />"); }
         public static IIconType Chart_infographic { get => new TablerIcon(@"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><circle cx='7' cy='7' r='4' /><path d='M7 3v4h4' /><line x1='9' y1='17' x2='9' y2='21' /><line x1='17' y1='14' x2='17' y2='21' /><line x1='13' y1='13' x2='13' y2='21' /><line x1='21' y1='12' x2='21' y2='21' />"); }
